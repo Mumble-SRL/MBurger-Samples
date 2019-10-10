ec89c72a67c44b8e60ea767f3b50594eb7c2a3f7
Alessandro Viviani <alessandro.viviani@mumbleideas.it>
2019-10-10 15:04:30 +0200

    push sample

---
+++ iOS/MBurgerNewsPush/MBurgerNewsPush/NotificationsController.swift
@@ -0,0 +1,9 @@
+//
+//  NotificationsController.swift
+//  MBurgerNewsPush
+//
+//  Created by Alessandro Viviani on 10/10/2019.
+//  Copyright © 2019 Mumble S.r.l (https://mumbleideas.it). All rights reserved.
+//
+
+import Foundation
---
+++ iOS/MBurgerNewsPush/MBurgerNewsPush/Settings/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  MBurgerNewsPush
+//
+//  Created by Alessandro Viviani on 10/10/2019.
+//  Copyright © 2019 Mumble S.r.l (https://mumbleideas.it). All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
